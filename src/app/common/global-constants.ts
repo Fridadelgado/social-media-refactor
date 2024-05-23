d7ad684f59bee7817555238b1bc5547875f755cd
fridaadel <frida.delgado@seekop.com>
2024-05-22 20:13:41 -0600

    consumiendo endpoint redes sociales

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,4 +1,5 @@
 export class GlobalConstants{
     public static urlApiCampanias = 'https://ti3pwepc47.execute-api.us-west-1.amazonaws.com/dev/campanas';
+    public static urlApiRedesSociales = 'https://ti3pwepc47.execute-api.us-west-1.amazonaws.com/dev/redessociales';
 
 }
---
+++ src/app/interfaces/redes-sociales.interface.ts
@@ -0,0 +1,10 @@
+export type ResponseRedesSociales = RedesSociales[]
+
+export interface RedesSociales {
+  idred: number
+  nombre: string
+  endpointapi: string
+  apikey: string
+  secretkey: string
+  icon: string
+}
