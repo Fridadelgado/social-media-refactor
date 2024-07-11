4f271425667b7e76387d4568f4e8f9850335f755
fridaadel <frida.delgado@seekop.com>
2024-07-10 22:08:35 -0600

    sincronizando payloads

--- src/app/interfaces/instagram.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface PayloadRedSocial {
-    email: string;
-    distribuidor: string;
-    title: string;
-    mediaBase64?: string;
-    description: string,
-    alt_text: string
-}
--- src/app/services/publicaciones.service.ts
+++ src/app/services/publicaciones.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of, tap } from 'rxjs';
 import { Campanias, CampaniasBody, GenericResponse } from '../interfaces/campanias.interface';
 import { GlobalConstants } from '../common/global-constants';
-import { PinterestPublicacion, ResponseRedesSociales } from '../interfaces/redes-sociales.interface';
-import { PayloadRedSocial } from '../interfaces/instagram.interface';
+import { ResponseRedesSociales } from '../interfaces/redes-sociales.interface';
+import { BasePayload, FacebookPayload, InstagramPayload, MediaPayload, PinterestPayload, TikTokPayload, TwitterPayload, YouTubePayload } from '../interfaces/red-social-payload.interface';
 
 export interface Publicacion {
   redSocial: string[];
@@ -30,87 +30,97 @@ export class PublicacionesService {
     return this._publicaciones.asObservable();
   }
 
-  agregarPublicacion(publicacion: Publicacion): Observable<any> {
-    publicacion.redSocial
-    let imageBase64 = publicacion.imagen;
-    const base64ImagePattern = /^data:image\/[a-z]+;base64,/;
+  private getPayloadAndUrl(publicacion: Publicacion, red: string): { payload: BasePayload | MediaPayload, url: string } {
+    console.log(red);
 
-    if (base64ImagePattern.test(imageBase64)) {
-      imageBase64 = imageBase64.split(',')[1];
-    }
-
-    const payload: PayloadRedSocial = {
+    const basePayload: BasePayload = {
       email: 'default.pruebas@seekop.com',
-      distribuidor: "104425",
-      title: publicacion.titulo,
-     // mediaBase64: imageBase64,
-      description: publicacion.descripcion,
-      alt_text: 'Texto descriptivo'
+      distribuidor: '104425'
+    };
+
+    switch (red.toLowerCase()) {
+      case 'twitter':
+        return {
+          payload: { ...basePayload, text: publicacion.titulo, mediaBase64: publicacion.imagen } as TwitterPayload,
+          url: `${GlobalConstants.urlApiPublicar}publicarentwitter`
+        };
+      case 'facebook':
+        return {
+          payload: { ...basePayload, text: publicacion.titulo, mediaBase64: publicacion.imagen } as FacebookPayload,
+          url: `${GlobalConstants.urlApiPublicar}publicarenfacebook`
+        };
+      case 'youtube':
+        return {
+          payload: { ...basePayload, text: publicacion.titulo, mediaBase64: publicacion.imagen } as YouTubePayload,
+          url: `${GlobalConstants.urlApiPublicar}publicarenyoutube`
+        };
+      case 'instagram':
+        return {
+          payload: { ...basePayload, text: publicacion.titulo, mediaBase64: publicacion.imagen } as InstagramPayload,
+          url: `${GlobalConstants.urlApiPublicar}publicareninstagram`
+        };
+      case 'tiktok':
+        return {
+          payload: { ...basePayload, text: publicacion.titulo } as TikTokPayload,
+          url: `${GlobalConstants.urlApiPublicar}publicarentiktok`
+        };
+      case 'pinterest':
+        return {
+          payload: { ...basePayload, title: publicacion.titulo, alt_text: publicacion.titulo, description: publicacion.descripcion } as PinterestPayload,
+          url: `${GlobalConstants.urlApiPublicar}publicarenpinterest`
+        };
+      default:
+        throw new Error(`Red social no soportada: ${red}`);
     }
-    const requests = publicacion.redSocial.map(red => {
-      console.log(payload);
-      switch (red.toLowerCase()) {
-        case 'twitter':
-          return this.publicarEnTwitter(payload);
-        case 'facebook':
-          return this.publicarEnFacebook(payload);
-        case 'youtube':
-          return this.publicarEnYouTube(payload);
-        case 'instagram':
-          return this.publicarEnInstagram(payload);
-        case 'tiktok':
-          return this.publicarEnTikTok(payload);
-        case 'pinterest':
-          return this.publicarEnPinterest(payload);
-        default:
-          return Promise.reject(`Red social no soportada: ${red}`);
-      }
-    });
+  }
+
+  private async publicarEnRedSocial(publicacion: Publicacion, red: string): Promise<any> {
+    const { payload, url } = this.getPayloadAndUrl(publicacion, red);
+    console.log(payload);
+    /*
+      if (payload.hasOwnProperty('mediaBase64') && payload.mediaBase64 && /^data:image\/[a-z]+;base64,/.test(payload.mediaBase64)) {
+        payload.mediaBase64 = payload.mediaBase64.split(',')[1];
+      }*/
+
+    try {
+      const response = await this.http.post(url, payload).toPromise();
+      return response;
+    } catch (error) {
+      console.error(`Error al publicar en ${red}:`, error);
+      throw error;
+    }
+  }
+
+
+  // Función principal para agregar una publicación en varias redes sociales
+  agregarPublicacion(publicacion: Publicacion): Observable<any> {
+    const requests = publicacion.redSocial.map(red => this.publicarEnRedSocial(publicacion, red));
 
     return new Observable(observer => {
-      Promise.all(requests).then(responses => {
-        console.log("este es el response ", responses);
+      Promise.all(requests)
+        .then(responses => {
+          console.log("Este es el response:", responses);
 
-        // Filtrar las respuestas exitosas de YouTube
-        const youtubeResponse = responses.find(response =>
-          response.statusCode === 200 && response.body && response.body.data && response.body.data.kind === 'youtube#video'
-        );
 
-        if (youtubeResponse && youtubeResponse.body.data.snippet.thumbnails) {
-          publicacion.thumbnail = youtubeResponse.body.data.snippet.thumbnails.high.url;
-          publicacion.link = `https://www.youtube.com/watch?v=${youtubeResponse.body.data.id}`;
-        }
+          // Filtrar las respuestas exitosas de YouTube
+          const youtubeResponse = responses.find(response =>
+            response.statusCode === 200 && response.body && response.body.data && response.body.data.kind === 'youtube#video'
+          );
 
-        this.actualizarPublicaciones(publicacion);
-        observer.next(responses);
-        observer.complete();
-      }).catch(error => {
-        console.error('Error al publicar en redes sociales', error);
-        observer.error(error);
-      });
+          if (youtubeResponse && youtubeResponse.body.data.snippet.thumbnails) {
+            publicacion.thumbnail = youtubeResponse.body.data.snippet.thumbnails.high.url;
+            publicacion.link = `https://www.youtube.com/watch?v=${youtubeResponse.body.data.id}`;
+          }
+
+          this.actualizarPublicaciones(publicacion);
+          observer.next(responses);
+          observer.complete();
+        })
+        .catch(error => {
+          console.error('Error al publicar en redes sociales:', error);
+          observer.error(error);
+        });
     });
-  }
-
-  private publicarEnTwitter(payload: any): Promise<any> {
-    return this.http.post(GlobalConstants.urlApiPublicar + `publicarentwitter`, payload).toPromise();
-  }
-
-  private publicarEnFacebook(payload: PayloadRedSocial): Promise<any> {
-    return this.http.post(GlobalConstants.urlApiPublicar + `publicarenfacebook`, payload).toPromise();
-  }
-
-  private publicarEnYouTube(payload: any): Promise<any> {
-    return this.http.post(GlobalConstants.urlApiPublicar + `publicarenyoutube`, payload).toPromise();
-  }
-  private publicarEnInstagram(payload: PayloadRedSocial): Promise<any> {
-    return this.http.post(GlobalConstants.urlApiPublicar + `publicareninstagram`, payload).toPromise();
-  }
-  //actualizar endpoint
-  private publicarEnTikTok(payload: PayloadRedSocial): Promise<any> {
-    return this.http.post(GlobalConstants.urlApiPublicar + `publicareninstagram`, payload).toPromise();
-  }
-  private publicarEnPinterest(payload: PayloadRedSocial): Promise<any> {
-    return this.http.post(GlobalConstants.urlApiPublicar + `publicarenpinterest`, payload).toPromise();
   }
 
   private actualizarPublicaciones(publicacion: Publicacion) {
